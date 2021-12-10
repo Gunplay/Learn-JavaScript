9d01411f1df1db8d1188ba586a515a7d3f213e9a
Yankers <vovarojko@.com>
2021-12-10 22:47:39 +0200

    2.16

--- 2.15 Функции/task.js
+++ 2.15 Функции/task.js
@@ -76,35 +76,27 @@
 // pow(1, 100) = 1 * 1 * ...* 1 = 1
 // Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
 //
-// function pow(x,n) {
-//
-//     const result = (a ** b);
-//     let double = prompt('Введите число и возведём в степень', '');
-//
-//
-//           alert(result);
-//
-// }
-//
-// pow();
-
 
 
 function pow(x, n) {
     let result = x;
 
     for (let i = 1; i < n; i++) {
-        result *= x;
+        result *= n;
+
     }
 
     return result;
+
+
 }
 
-let x = prompt("x?", '');
-let n = prompt("n?", '');
+let x = prompt('x?', '');
+let n = prompt('n?', '');
 
-if (n < 1) {
-    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+if ( n < 1 ) {
+ alert(`Степень ${n} не поддерживается, используйте натуральное число`);
 } else {
-    alert(pow(x, n));
+    console.log(pow(x,n));
 }
+
